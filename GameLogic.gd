4890a8a68bc57aed36ba6f30bcce7e5aba1f94f3
Timothy Stiles <Patashu@hotmail.com>
2023-12-12 17:37:08 +1100

    more oob wiring fixes

--- GameLogic.gd
+++ GameLogic.gd
@@ -1069,7 +1069,7 @@ func change_pressed(actor: Actor, new_value: bool, pos: Vector2, chrono: int) ->
 	set_actor_var(actor, "pressing", new_value, chrono);
 	var button_id = reverse_buttons[pos];
 	for wire in wires:
-		if wire[0] == button_id and hatches.size() > wire[1]:
+		if wire[0] == button_id and int(wire[1]) == wire[1] and hatches.size() > wire[1] and -hatches.size() <= wire[1]:
 			set_actor_var(hatches[wire[1]], "open", new_value, chrono);
 
 func make_actor(actorname: int, pos: Vector2, is_character: bool, chrono: int = Chrono.TIMELESS) -> Actor:
